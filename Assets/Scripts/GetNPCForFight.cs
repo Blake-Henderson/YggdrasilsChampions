2f23086d5ad1c2b6a814ddca19afd77912fd9bb1
Blake-Henderson <bmhenderson101@gmail.com>
2023-02-05 02:44:57 -0600

    Prevents NPC leveling

--- Assets/Scripts/GetNPCForFight.cs
+++ Assets/Scripts/GetNPCForFight.cs
@@ -12,7 +12,12 @@ public class GetNPCForFight : MonoBehaviour
     public Character GetCharacter()
     {
         Character temp = transform.GetChild(Random.Range(0, transform.childCount)).GetComponent<Character>();
-        temp.stats.health = temp.stats.hp; 
+        temp.stats.hp = temp.stats.baseStats.hp;
+        temp.stats.health = temp.stats.baseStats.hp;
+        temp.stats.attack = temp.stats.baseStats.attack;
+        temp.stats.defense = temp.stats.baseStats.defense;
+        temp.stats.speed = temp.stats.baseStats.speed;
+        temp.stats.evade = temp.stats.baseStats.evade;
         return temp;
     }
 }
