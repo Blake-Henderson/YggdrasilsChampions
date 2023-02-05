bc50bbc36ae363cb12e71bdc2241e80c6d5be92e
Blake-Henderson <bmhenderson101@gmail.com>
2023-02-05 15:11:03 -0600

    music implemented

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    public GameObject track1;
+    public GameObject track2;
+    public static DontDestroy instance;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        track1.SetActive(true);
+        track2.SetActive(false);
+    }
+
+    public void swapTracks()
+    {
+        track1.SetActive(track2.activeInHierarchy);
+        track2.SetActive(!track1.activeInHierarchy);
     }
 }
